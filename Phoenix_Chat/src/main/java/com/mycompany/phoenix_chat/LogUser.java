b2b220b609ba927a89caef9b2b8e1157b2de86ef
Ndimuhulu Matamela <st10495492@rcconnect.edu.za>
2025-04-13 16:23:47 +0000

    Add files via upload

---
+++ Phoenix_Chat/src/main/java/com/mycompany/phoenix_chat/LogUser.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.phoenix_chat;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class LogUser {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+    }}
+
+    
+    
+
